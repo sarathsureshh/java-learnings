9ea13a5061b0f35994ebed30e50dc8b4e370077f
sarathsuresh <sarath.suresh@tcs.com>
2022-11-20 11:30:05 +0530

    Conditionals continued

--- Conditionals/src/Conditionals.java
+++ Conditionals/src/Conditionals.java
@@ -3,15 +3,26 @@ import java.util.*;
 public class Conditionals {
     public static void main(String[] args) {
 
-        ifTriangle(checkIfTriangle());
-        multiplicationTable();
-        evenNumbers();
+//        ifTriangle(checkIfTriangle());
+//        multiplicationTable();
+//        evenNumbers();
+//        print1to10();
+//        print10to1();
+        squares();
+        squaresOfOdd();
+        squaresOfEven();
 
     }
+
+
+
     public static void ifTriangle(boolean isTrangle) {
         if (isTrangle) {
-            System.out.println("Yesssss it is a TRANGLE");
-        } else System.out.println("Ah shoot not a triangle");
+            System.out.println("Yesssss it is a TRIANGLE");
+        } else
+        {
+            System.out.println("Ah shoot not a triangle");
+        }
     }
 
     public static boolean checkIfTriangle() {
@@ -44,11 +55,59 @@ public class Conditionals {
             System.out.println(divisor + " x " + i + " = " + res);
         }
     }
-
     public static void evenNumbers() {
         for (int i = 0; i <= 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
+            }
+        }
+    }
+
+    public static void print1to10()
+    {
+        for(int i=1; i<=10; i++ )
+        {
+            System.out.println(i);
+        }
+    }
+    public static void print10to1()
+    {
+        for(int i=10; i>=1; i-- )
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void squares()
+    {
+        System.out.println("Printing the squares of first 10 Numbers!");
+        for(int i = 1; i<=10; i++)
+        {
+            System.out.println(i*i);
+        }
+    }
+
+    public static void squaresOfOdd()
+    {
+        System.out.println("Printing the squares of first 10 Odd Numbers!");
+        for(int i =0; i<=20; i++ )
+        {
+            if (i%2!=0)
+            {
+                System.out.println(i*i);
+            }
+        }
+
+    }
+
+    private static void squaresOfEven() {
+
+        System.out.println("Printing the squares of first 10 Even Numbers!");
+        for(int i =0; i<=20; i++ )
+        {
+            if (i%2==0)
+            {
+                System.out.println(i*i);
             }
         }
     }
